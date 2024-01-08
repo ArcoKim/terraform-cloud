1c22b5d52e6370896b99ffdd338bd3870ccd01db
Jeongtae Kim <arcomade12@gmail.com>
2024-01-08 15:52:37 +0900

    merge error fixed

--- AWS/skills/2021-seoul/asg.tf
+++ AWS/skills/2021-seoul/asg.tf
@@ -72,7 +72,21 @@ resource "aws_launch_template" "stable" {
     }
   }
 
-  user_data = filebase64("${local.filepath}/stable.sh")
+  user_data = base64encode(<<EOF
+    #!/bin/bash
+    sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+    systemctl restart sshd
+    echo 'ec25678!' | passwd --stdin ec2-user
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    aws s3 cp s3://${aws_s3_bucket.static-s3.id}/web/v1.tar .
+    tar -xvf v1.tar -C /var/www/html/
+    mv /var/www/html/v1/* /var/www/html
+    rm -rf /var/www/html/v1
+  EOF
+  )
 }
 
 resource "aws_launch_template" "test" {
@@ -93,7 +107,21 @@ resource "aws_launch_template" "test" {
     }
   }
 
-  user_data = filebase64("${local.filepath}/test.sh")
+  user_data = base64encode(<<EOF
+    #!/bin/bash
+    sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+    systemctl restart sshd
+    echo 'ec25678!' | passwd --stdin ec2-user
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    aws s3 cp s3://${aws_s3_bucket.static-s3.id}/web/v2.tar .
+    tar -xvf v2.tar -C /var/www/html/
+    mv /var/www/html/v2/* /var/www/html
+    rm -rf /var/www/html/v2
+  EOF
+  )
 }
 
 resource "aws_security_group" "web" {
